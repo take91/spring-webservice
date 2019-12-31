786b553879205188e9fc8ada96b1e209829dd2cd
Gonhong <sarkh@Gonhongui-MacBookPro.local>
2019-12-31 15:18:56 +0900

    first commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+## A Webservice Practice by Spring boot and Gradle
+
+<hr>
+<h2>Made by..</h2>
+<li>Spring Boot</li>
+<li>Gradle</li>
+<li>H2</li>
---
+++ src/test/java/com/take/spring/SpringWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.take.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
